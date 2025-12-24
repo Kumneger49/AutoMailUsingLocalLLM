0bac145801d49234ec53bc8cd2e5ab159c9983ed
kumneger49 <kumnegermarkos49@gmail.com>
2025-12-24 12:02:52 +0300

    write a basic script to choose the model based on accuracy and inference time

---
+++ test.py
@@ -0,0 +1,17 @@
+
+from ollama import generate
+import time
+# Connect to your local Ollama instance
+email_content = "Hi Kumneger, can you send me the latest project report by today?"
+
+prompt = f"""
+Instruction: Write a concise, professional reply to this email.
+Email: "{email_content}"
+"""
+# Call your local model
+start_time = time.time()
+response = generate(model="llama3.2:latest", prompt=prompt)
+end_time = time.time()
+print("Draft reply:\n", response.response)
+print(f"took: {end_time-start_time}:.2f")
+
